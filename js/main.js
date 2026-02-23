6ed1d2220542f59bc6aa4bb8d661a4bc6f5ef77c
Valeria Villa <vvilla@caltech.edu>
2026-02-23 14:01:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // /js/main.js
 
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 // ---- DOM container ----
 const container = document.getElementById("container3D");
